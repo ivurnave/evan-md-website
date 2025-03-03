32c500a5fb396c94b55c94fb8dd7391d384d9723
ivurnave <ivur.nave@gmail.com>
2025-03-02 22:54:10 -0600

    change border radius for panels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { MarkdownPage } from "@/components/MarkdownPage";
 const Home = () => {
     return (
         <>
-            <div className="mb-4 p-2 rounded-[2vw] border-solid border border-slate-50 shadow-xl">
+            <div className="mb-4 p-2 rounded-3xl border-solid border border-slate-50 shadow-xl">
                 <div className="flex flex-col md:flex-row items-center gap-x-6">
                     <h1 className="block text-center md:text-left text-6xl">evan holmes</h1>
                     <img className="dark:invert w-1/2" src="https://ivurnave.github.io/me/static/media/cube.b476af13.png" />
