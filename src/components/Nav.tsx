55a946f71763e85096454c695d5c7a60f0021784
ivurnave <ivur.nave@gmail.com>
2025-03-08 14:10:09 -0600

    mobile nav menu styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,57 @@ const chooseHoverColor = () => {
     return colors[randomIndex];
 }
 
-const StyledLink = ({ href, title }: { href: string, title: string }) => <Link className={`mx-1 p-1 rounded-full hover:bg-coral-light`} href={href}>{title}</Link>
+const StyledLink = ({ href, title }: { href: string, title: string }) => <Link className={`m-1 p-1 rounded-full hover:bg-coral-light`} href={href}>{title}</Link>
+
+const ToggleNavbarButton = ({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: (val: boolean) => void }) => {
+
+    const toggleNavbar = () => setIsOpen(!isOpen)
+    return (
+        // <button onClick={toggleNavbar}
+        //     className="relative flex flex-col size-[32px] rounded-full justify-between items-center border-solid border border-outline shadow-xl overflow-hidden">
+        //     {/* <span className={`bg-foreground block transition-all duration-300 ease-out 
+        //              w-3 rounded-sm ${isOpen ?
+        //             'rotate-45 translate-y-1' : '-translate-y-0.5'
+        //         }`} >
+        //     </span>
+        //     <span className={`bg-foreground block transition-all duration-300 ease-out 
+        //              w-6 rounded-sm ${isOpen ?
+        //             'opacity-0' : 'opacity-100'
+        //         }`} >
+        //     </span>
+        //     <span className={`bg-foreground block transition-all duration-300 ease-out 
+        //              w-6 rounded-sm ${isOpen ?
+        //             '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+        //         }`} >
+        //     </span> */}
+        //     <span
+        //         className={`bg-foreground block transition-all duration-300 ease-out w-6 h-0.5 rounded-sm transform ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-1'}`}
+        //     ></span>
+        //     <span
+        //         className={`bg-foreground block transition-all duration-300 ease-out w-6 h-0.5 rounded-sm ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+        //     ></span>
+        //     <span
+        //         className={`bg-foreground block transition-all duration-300 ease-out w-6 h-0.5 rounded-sm transform ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-1'}`}
+        //     ></span>
+
+        // </button>
+        <button
+            onClick={toggleNavbar}
+            className="relative size-[32px] flex flex-col justify-center items-center rounded-full border-solid border border-outline shadow-xl overflow-hidden"
+        >
+            <span
+                className={`bg-foreground block transition-all duration-300 ease-out w-[16px] h-[1px] rounded-sm transform ${isOpen ? 'rotate-45 translate-y-[1px]' : '-translate-y-[4px]'}`}
+            ></span>
+            <span
+                className={`bg-foreground block transition-all duration-300 ease-out w-[16px] h-[1px] rounded-sm ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+            ></span>
+            <span
+                className={`bg-foreground block transition-all duration-300 ease-out w-[16px] h-[1px] rounded-sm transform ${isOpen ? '-rotate-45 -translate-y-[1px]' : 'translate-y-[4px]'}`}
+            ></span>
+        </button>
+
+    )
+}
 
 export const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -43,31 +93,5 @@ export const Nav = () => {
                     </div>}
             </div>
         </>
-    )
-}
-
-const ToggleNavbarButton = ({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: (val: boolean) => void }) => {
-
-    const toggleNavbar = () => setIsOpen(!isOpen)
-    return (
-        <button onClick={toggleNavbar}
-            className="flex flex-col size-[32px] rounded-full justify-center items-center border-solid border border-outline shadow-xl overflow-hidden">
-            <span className={`bg-foreground block transition-all duration-300 ease-out 
-                     w-6 rounded-sm ${isOpen ?
-                    'rotate-45 translate-y-1' : '-translate-y-0.5'
-                }`} >
-            </span>
-            <span className={`bg-foreground block transition-all duration-300 ease-out 
-                     w-6 rounded-sm my-0.5 ${isOpen ?
-                    'opacity-0' : 'opacity-100'
-                }`} >
-            </span>
-            <span className={`bg-foreground block transition-all duration-300 ease-out 
-                     w-6 rounded-sm ${isOpen ?
-                    '-rotate-45 -translate-y-1' : 'translate-y-0.5'
-                }`} >
-            </span>
-
-        </button>
     )
 }
