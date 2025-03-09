0749b4af07107d425433cc7947b1a8615365ae37
ivurnave <ivur.nave@gmail.com>
2025-03-08 22:36:53 -0600

    fix wider spacing for page

--- src/components/MarkdownPage.tsx
+++ src/components/MarkdownPage.tsx
@@ -5,7 +5,7 @@ export const MarkdownPage = (props: { markdownFileName: string }) => {
   const mdContent = getMarkdownContent(props.markdownFileName);
   return (
     <div className="p-2 rounded-3xl border-solid border border-outline shadow-xl flex flex-col items-center">
-      <Markdown className="max-w-2xl">{mdContent}</Markdown>
+      <Markdown className="w-full max-w-3xl">{mdContent}</Markdown>
     </div>
   );
 } 
