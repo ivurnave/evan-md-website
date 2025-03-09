ab6985a8cceafb6687bc2a0bd9ed5780c231c709
Evan Holmes <ivur.nave@gmail.com>
2025-03-08 22:45:38 -0600

    contact me page (#3)
    * move contact info to contact me page, restructure home page
    
    * fix image, linting
    
    * fix wider spacing for page
    
    * more spacing changes

--- markdown/bio.md
+++ markdown/bio.md
@@ -1,6 +1,2 @@
 # Hi! I'm Evan :)
 I work as a Sr. Software Engineer at Target in Minneapolis. I have many hobbies, including painting, woodworking, pottery, making and playing video games, and more. This site serves as a living resume for me and my work over the years. When I remember to, I update this site with my latest projects.
-<br/><br/>
-
-## Contact
-If you'd like to contact me, you can [send me an email](mailto:ivur.nave@gmail.com) or you can find me on [LinkedIn](https://www.linkedin.com/in/evan-holmes-4b108760/)
--- src/components/MarkdownPage.tsx
+++ src/components/MarkdownPage.tsx
@@ -4,8 +4,10 @@ import { getMarkdownContent } from "../helpers";
 export const MarkdownPage = (props: { markdownFileName: string }) => {
   const mdContent = getMarkdownContent(props.markdownFileName);
   return (
-    <div className="shrink p-2 rounded-3xl border-solid border border-outline shadow-xl">
-      <Markdown>{mdContent}</Markdown>
+    <div className="p-2 rounded-3xl border-solid border border-outline shadow-xl flex flex-col items-center">
+      <div className="w-full md:px-[20px]">
+        <Markdown>{mdContent}</Markdown>
+      </div>
     </div>
   );
 } 
