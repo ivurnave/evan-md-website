460ec24348bf6a283b2b78f8c8649e7af3509ed6
ivurnave <ivur.nave@gmail.com>
2025-03-08 22:44:03 -0600

    more spacing changes

--- src/components/MarkdownPage.tsx
+++ src/components/MarkdownPage.tsx
@@ -5,7 +5,9 @@ export const MarkdownPage = (props: { markdownFileName: string }) => {
   const mdContent = getMarkdownContent(props.markdownFileName);
   return (
     <div className="p-2 rounded-3xl border-solid border border-outline shadow-xl flex flex-col items-center">
-      <Markdown className="w-full max-w-3xl">{mdContent}</Markdown>
+      <div className="w-full md:px-[20px]">
+        <Markdown>{mdContent}</Markdown>
+      </div>
     </div>
   );
 } 
